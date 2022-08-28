dfd130914bcc18b44221e75be6c85574e2127424
phgrosjean <phgrosjean@sciviews.org>
2022-08-28 19:53:43 +0200

    duvkdb eliminated from packages installed in svbox2022 (because 50Mb)

--- svbox2022/scripts/install_sciviews.sh
+++ svbox2022/scripts/install_sciviews.sh
@@ -45,32 +45,32 @@ install2.r --error --skipinstalled -n "$NCPUS" \
     butcher ca car circular cli clisymbols collapse config conflicted convertr \
     coro corrplot corrr covr cowplot crayon curl cyphr data.table datasauRus \
     DBI dbplyr devtools DiagrammeR dials digest distributional distributions3 \
-    doFuture doParallel doRNG dm DT dtplyr duckdb e1071 egg ellipse esquisse \
-    evaluate factoextra FactoMineR fansi faraway fastcluster fastmap feasts \
-    flashClust flexdashboard flipdownr fontawesome forcats foreach formatR \
-    fortunes fs furrr future future.apply future.callr gdtools generics \
-    getPass GGally gganimate ggconf ggcorrplot ggdendro ggExtra ggfortify \
-    ggiraph ggpackets ggpubr ggRandomForests ggrepel ggridges ggsci ggsignif \
-    ggsn ggsom ghclass gitcreds glmnet glue golem googlesheets4 gridBase \
-    gridExtra gridGraphics gstat haven here Hmisc htmltools htmlwidgets httpuv \
-    httr igraph inline ipred IRkernel iterators janitor job jsonlite keyring \
-    knitr knitcitations kohonen later lattice latticeExtra leaflet lifecycle \
-    lindia lintr lobstr lme4 lmerTest lubridate magrittr mapedit maps mapsf \
-    mapview markdown memoise microbenchmark mime miniCRAN miniUI mlbench \
-    modelr mongolite mosaic multcomp mvtnorm naniar nanotime nparcomp odbc \
-    pagedown pak palmerpenguins pander parsnip patchwork piggyback pillar pins \
-    pkgdepends pkgdown PKI plotly prettyglm printr pROC progressr promises \
-    pryr purrr pwr quantreg quarto questionr r3js R6 ragg randomForest ranger \
-    raster RColorBrewer Rcpp RcppCCTZ reactlog readxl recipes rhub remotes \
-    reprex reticulate rgl RhpcBLASctl rlang rmarkdown rmdformats ROCR rpart \
-    rprojroot rsample rsconnect RSQLite rticles sessioninfo shiny \
-    shinydashboard shinyjs shinylogs shinytest2 shinytoastr shinyWidgets skimr \
-    slider sloop smotefamily sodium sparklyr spData spelling stars stringi \
-    stringr styler summaryTools suppdata SuppDists svglite svUnit sys targets \
-    testthat thematic tictoc tidymodels tidyr tidyverse tinytest tmap \
-    tmaptools todor TSA tsibble tune usethis UsingR vctrs vegan vembedr \
-    vetiver vip viridis viridisLite visdat vroom waldo warp withr workflows \
-    workflowsets writexl xfun xaringan xtable xts yaml yardstick zeallot zoo
+    doFuture doParallel doRNG dm DT dtplyr e1071 egg ellipse esquisse evaluate \
+    factoextra FactoMineR fansi faraway fastcluster fastmap feasts flashClust \
+    flexdashboard flipdownr fontawesome forcats foreach formatR fortunes fs \
+    furrr future future.apply future.callr gdtools generics getPass GGally \
+    gganimate ggconf ggcorrplot ggdendro ggExtra ggfortify ggiraph ggpackets \
+    ggpubr ggRandomForests ggrepel ggridges ggsci ggsignif ggsn ggsom ghclass \
+    gitcreds glmnet glue golem googlesheets4 gridBase gridExtra gridGraphics \
+    gstat haven here Hmisc htmltools htmlwidgets httpuv httr igraph inline \
+    ipred IRkernel iterators janitor job jsonlite keyring knitr knitcitations \
+    kohonen later lattice latticeExtra leaflet lifecycle lindia lintr lobstr \
+    lme4 lmerTest lubridate magrittr mapedit maps mapsf mapview markdown \
+    memoise microbenchmark mime miniCRAN miniUI mlbench modelr mongolite \
+    mosaic multcomp mvtnorm naniar nanotime nparcomp odbc pagedown pak \
+    palmerpenguins pander parsnip patchwork piggyback pillar pins pkgdepends \
+    pkgdown PKI plotly prettyglm printr pROC progressr promises pryr purrr pwr \
+    quantreg quarto questionr r3js R6 ragg randomForest ranger raster \
+    RColorBrewer Rcpp RcppCCTZ reactlog readxl recipes rhub remotes reprex \
+    reticulate rgl RhpcBLASctl rlang rmarkdown rmdformats ROCR rpart rprojroot \
+    rsample rsconnect RSQLite rticles sessioninfo shiny shinydashboard shinyjs \
+    shinylogs shinytest2 shinytoastr shinyWidgets skimr slider sloop \
+    smotefamily sodium sparklyr spData spelling stars stringi stringr styler \
+    summaryTools suppdata SuppDists svglite svUnit sys targets testthat \
+    thematic tictoc tidymodels tidyr tidyverse tinytest tmap tmaptools todor \
+    TSA tsibble tune usethis UsingR vctrs vegan vembedr vetiver vip viridis \
+    viridisLite visdat vroom waldo warp withr workflows workflowsets writexl \
+    xfun xaringan xtable xts yaml yardstick zeallot zoo
 
 # SciViews extensions
 R -e "options(repos = c(
